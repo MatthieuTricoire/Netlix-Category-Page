09d00fc4df44d02509509de32938e0f1e3a3127f
MatthieuTricoire <matthieutricoire@gmail.com>
2023-01-31 15:26:11 +0000

    component section

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -5,12 +5,14 @@ const Section = (props) => {
     <section>
       {data.map((moviesByCategories, index) => {
         <div className="category" key={index}>
+          {console.log(moviesByCategories.category)}
           <h2 key={index}>{moviesByCategories.category}</h2>
         </div>;
         <div className="thumbnails">
           {moviesByCategories.images.map((thumbnails, index) => {
             <div className="thumbnails">
-              <img src="thumbnail" alt="" />
+              {console.log(thumbnails)}
+              <img src={thumbnails} alt="" />
             </div>;
           })}
         </div>;
